28c4455b9561deb2d1ba819df866670a009b134b
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2022-08-04 23:26:11 +0200

    news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -19,7 +19,7 @@ const News = ({ simplified }) => {
   if (!cryptoNews?.value) return <Loader />;
 
   return (
-    <Row gutter={[24,24]}>
+    <Row gutter={[24, 24]}>
       {!simplified && (
         <Col span={24}>
           <Select
@@ -35,38 +35,28 @@ const News = ({ simplified }) => {
           </Select>
         </Col>
       )}
-
-      {cryptoNews.value.map((news,i)=>(
-        <Col xs={24} sm={12} lg={8}  key={i}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
-              <a href={news.url} target="_blank" rel='noreferrer'>
-                <div style={{maxHeight:'100px' ,maxWidth:'200px'}} className="news-image-container">
-                    <Title className="news-title"  level={5}>
-                    {news.name}
-                  </Title>
-                  <img src={news?.image?.thumbnail?.contentUrl || demoImage}  alt="news"/>
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>{news.name}</Title>
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+              </div>
+              <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                  <Text className="provider-name">{news.provider[0]?.name}</Text>
                 </div>
-                <p>
-                  {news.description > 100 
-                  ? `${news.description.substring(0,100)}....`
-                  : news.description
-                  }
-                </p>
-                <div className="provider-container">
-                  <div>
-                    <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
-                    <Text  className="provider-name">{news.provider[0]?.name}</Text>
-                  </div>
-                  <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
-
-                </div>
-              </a>
+                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
           </Card>
         </Col>
       ))}
-
     </Row>
-  )
+  );
 };
 
 export default News;
